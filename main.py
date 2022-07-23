5cf3e39899379d189d5a4f7d2e18e2601e3e55fc
dhruv50ae <linkdp2012@gmail.com>
2022-07-23 16:14:42 +0100

    Final Project complete

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
+
+def genPass():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_list = []
+
+    for char in range(nr_letters):
+        password_list.append(random.choice(letters))
+
+    for char in range(nr_symbols):
+        password_list += random.choice(symbols)
+
+    for char in range(nr_numbers):
+        password_list += random.choice(numbers)
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+    pyperclip.copy(password)
+    passEntry.insert(0, password)
 
 
 def save():
@@ -46,7 +76,7 @@ euNameEntry.insert(0, "Fryost50AE")
 passEntry = Entry(width=17)
 passEntry.grid(row=3, column=1)
 
-genPassButton = Button(text="Generate Password")
+genPassButton = Button(text="Generate Password", command=genPass)
 genPassButton.grid(row=3, column=2)
 addButton = Button(text="Add", width=30, command=save)
 addButton.grid(row=4, column=1, columnspan=2)
