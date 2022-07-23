3b587df7bc40221ca361066164b1b2a570b7382a
dhruv50ae <linkdp2012@gmail.com>
2022-07-23 15:49:33 +0100

    Create logic

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from tkinter import *
 
+
+def save():
+    websiteCreds = webEntry.get()
+    euNameCreds = euNameEntry.get()
+    passCreds = passEntry.get()
+    with open("data.txt", "a") as data:
+        data.write(f"{websiteCreds} | {euNameCreds} | {passCreds}\n")
+    webEntry.delete(0, END)
+    passEntry.delete(0, END)
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -18,14 +28,16 @@ passLabel.grid(row=3, column=0)
 
 webEntry = Entry(width=35)
 webEntry.grid(row=1, column=1, columnspan=2)
+webEntry.focus()
 euNameEntry = Entry(width=35)
 euNameEntry.grid(row=2, column=1, columnspan=2)
+euNameEntry.insert(0, "Fryost50AE")
 passEntry = Entry(width=17)
 passEntry.grid(row=3, column=1)
 
 genPassButton = Button(text="Generate Password")
 genPassButton.grid(row=3, column=2)
-addButton = Button(text="Add", width=30)
+addButton = Button(text="Add", width=30, command=save)
 addButton.grid(row=4, column=1, columnspan=2)
 
 
